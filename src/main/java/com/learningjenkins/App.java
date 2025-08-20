c4e4bbbbd113d2c82a310d14d6b5d75127a566d4
KMohanVarma <mohanvarma.b@gmail.com>
2025-08-21 00:45:20 +0530

    Update App.java

--- src/main/java/com/learningjenkins/App.java
+++ src/main/java/com/learningjenkins/App.java
@@ -6,8 +6,8 @@ package com.learningjenkins;
  */
 public class App
 {
-    public static void main(String[] args)
+    public static String main()
     {
-        System.out.println("Hello World!");
+        return "Hello World!";
     }
 }
